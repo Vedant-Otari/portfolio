01013eb0dd6840bc389b353b6073af5c3d6933eb
Vedant Otari <vedantmotari24@gmail.com>
2023-12-04 23:18:44 +0530

    Added framer motion library.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { motion } from "framer-motion";
+import img from "../src/assests/images/hero-img.jpg";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="hero-text">
+        <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+          <h1>I'm Vedant Otari</h1>
+        </motion.div>
+        <h6>CSE Engineer</h6>
+      </div>
+      <div>
+        <img src={img} alt="Hero" />
+      </div>
     </div>
   );
 }
