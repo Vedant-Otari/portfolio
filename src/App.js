99fbd38d3550cf296c3dcb0ef5d6a2103ff1229a
Vedant Otari <vedantmotari24@gmail.com>
2023-12-06 12:28:30 +0530

    hero section edit.

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import Hero from "./components/Hero";
 
 function App() {
   return (
-    <>
-    <Header/>
-    <About/>
-    <Skills/>
-    <Projects/>
-    <Contact/>
-    </>
+    <div className="relative">
+      <Header />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+    </div>
   );
 }
 
