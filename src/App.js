961a8d17a9960973c9811dd9f2463c624dd2426d
Vedant Otari <vedantmotari24@gmail.com>
2023-12-08 12:38:25 +0530

    Mobile device about section added.

--- src/App.js
+++ src/App.js
@@ -4,17 +4,27 @@ import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Hero from "./components/Hero";
+import { motion } from "framer-motion";
 
 function App() {
   return (
-    <div className="relative">
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+      className="relative"
+    >
       <Header />
-      <Hero />
-      <About />
-      <Skills />
-      <Projects />
-      <Contact />
-    </div>
+      <main>
+        <Hero />
+        <About />
+        <Skills />
+        <Projects />
+      </main>
+      <footer>
+        <Contact />
+      </footer>
+    </motion.div>
   );
 }
 
