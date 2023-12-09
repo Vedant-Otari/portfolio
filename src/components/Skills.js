e02cfdd71f40864b20df11030266e87b43101e34
Vedant Otari <vedantmotari24@gmail.com>
2023-12-09 16:27:33 +0530

    Updates

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React from "react";
+import { FaReact } from "react-icons/fa";
 
 export default function Skills() {
   return (
-    <div>Skills</div>
-  )
+    <div className="py-3 px-3 bg-gray-200">
+      <h2 className="text-3xl py-4 text-center">My Skills</h2>
+      <section className="grid grid-cols-2 gap-3">
+        <div className="hover:shadow-[20px] duration-500 flex bg-white pt-4 pb-2 rounded-lg shadow-inner shadow-black flex-col items-center w-full">
+          <FaReact className=""/>
+          <span className="text-2xl">C</span>
+        </div>
+        <div className="hover:shadow-[20px] duration-500 flex bg-white pt-4 pb-2 rounded-lg shadow-inner shadow-black flex-col items-center w-full">
+          <FaReact className=""/>
+          <span className="text-2xl">CPP</span>
+        </div>
+        <div className="hover:shadow-[20px] duration-500 flex bg-white pt-4 pb-2 rounded-lg shadow-inner shadow-black flex-col items-center w-full">
+          <FaReact className=""/>
+          <span className="text-2xl">Javascript</span>
+        </div>
+        <div className="hover:shadow-[20px] duration-500 flex bg-white pt-4 pb-2 rounded-lg shadow-inner shadow-black flex-col items-center w-full">
+          <FaReact className=""/>
+          <span className="text-2xl">React</span>
+        </div>
+        <div className="hover:shadow-[20px] duration-500 flex bg-white pt-4 pb-2 rounded-lg shadow-inner shadow-black flex-col items-center w-full">
+          <FaReact className=""/>
+          <span className="text-2xl">Git</span>
+        </div>
+      </section>
+    </div>
+  );
 }
