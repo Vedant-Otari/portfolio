e5263fc53c6c2add9ffdd699e69c66b0a88f408b
Vedant Otari <vedantmotari24@gmail.com>
2023-12-14 10:08:41 +0530

    Project section carousal code added.

---
+++ src/assests/utils/projects.js
@@ -0,0 +1,8 @@
+import bestdeal from '../images/BestDeal.jpg'
+
+export const projects = [
+    {image: bestdeal, name:'Bestdeal1', description:'this is bestdeal1', tech:['react','mongoDB','Tailwind']},
+    {image: bestdeal, name:'Bestdeal2', description:'this is bestdeal2', tech:['react','mongoDB','Tailwind']},
+    {image: bestdeal, name:'Bestdeal3', description:'this is bestdeal3', tech:['react','mongoDB','Tailwind']},
+    {image: bestdeal, name:'Bestdeal4', description:'this is bestdeal4', tech:['react','mongoDB','Tailwind']},
+];
